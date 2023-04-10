37414c09aaeaedf6df15b9671202e9134b39a07e
elmaxis <maximiliano.88@hotmail.com>
2023-04-09 22:43:42 -0300

    avance

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -6,8 +6,8 @@ import Form from '../Components/Form'
 const Contact = () => {
   return (
     <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+      <h2>¿Querés saber más?</h2>
+      <p>Envíanos tus datos y nos pondremos en contacto con vos. </p>
       <Form/>
     </div>
   )
